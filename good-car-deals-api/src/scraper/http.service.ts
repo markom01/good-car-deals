1bd46b44b66e20b1dda953ed26d196e5d0b4a6a9
Marko <markomesa01@gmail.com>
2026-05-13 13:16:56 +0200

    add full browser headers to http service

--- good-car-deals-api/src/scraper/http.service.ts
+++ good-car-deals-api/src/scraper/http.service.ts
@@ -14,6 +14,16 @@ export class HttpService {
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'sr,en-US;q=0.9,en;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Upgrade-Insecure-Requests': '1',
+    Connection: 'keep-alive',
   };
   private readonly impit: Impit;
 
